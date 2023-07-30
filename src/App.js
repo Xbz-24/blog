268c58f0445b042ef6b25dbc6d354999786e0627
renato <renatochavez7@gmail.com>
2023-07-30 23:09:53 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BlogPost from './components/BlogPost';
+import './components/BlogPost.css'; // Import the CSS file
 
 function App() {
+  const blogPosts = [
+    {
+      title: 'My First Blog Post',
+      content: 'This is the content of my first blog post.',
+    },
+    {
+      title: 'Another Blog Post',
+      content: 'This is another blog post with some more content.',
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Your header content here */}
       </header>
+      <div className="App-content">
+        {blogPosts.map((post, index) => (
+          <BlogPost key={index} title={post.title} content={post.content} />
+        ))}
+      </div>
+      {/* Your footer content here */}
     </div>
   );
 }
