9788d283a55acf2f0417452872d9c34eab06ec70
renato <renatochavez7@gmail.com>
2023-07-31 03:50:52 +0200

    changes

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const api = axios.create({
+  baseUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:5119':'https://blog-2e5.pages.dev'
+});
+
+
 const BlogPosts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     // Fetch data from the backend API
-    axios.get('/api/posts')
+    api.get('/api/posts')
       .then(response => {
             console.log(response.data); // Add this line
         setPosts(response.data);
