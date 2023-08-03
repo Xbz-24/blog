7b3cf5401b706cb8f9c06f3bcf02201959b5e309
renato <renatochavez7@gmail.com>
2023-08-03 06:22:09 +0200

    created better front end design

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import NewPostForm from './NewPostForm';
 import BlogPosts from './BlogPost';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>My Blog</h1>
-      <NewPostForm />
-      <BlogPosts />
+      <div className="sidebar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          {/* Add more links as needed */}
+        </ul>
+      </div>
+      <div className="main-content">
+        <NewPostForm />
+        <BlogPosts />
+      </div>
     </div>
   );
 }
