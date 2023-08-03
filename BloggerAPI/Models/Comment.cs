2968c3d1e04bb892e03d8dd120320ba10a7477da
renato <renatochavez7@gmail.com>
2023-08-03 06:06:30 +0200

    added more backend models

--- BloggerAPI/Models/Comment.cs
+++ BloggerAPI/Models/Comment.cs
@@ -1,1 +1,9 @@
-using Sytem.IO;
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public string AuthorName { get; set; }
+    public DateTime DateCreated { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
--- BloggerAPI/Models/Post.cs
+++ BloggerAPI/Models/Post.cs
@@ -1,15 +1,12 @@
-namespace BloggerAPI.Models
+public class Post
 {
-    public class Post
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public Post()
-        {
-            Title = string.Empty;
-            Content = string.Empty;
-        }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string AuthorId { get; set; }
+    public Author Author { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateUpdated { get; set; }
+    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    public ICollection<PostTag> PostTags { get; set; } = new List<PostTag>();
 }
